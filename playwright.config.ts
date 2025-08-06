4a1f86dcf97053d2beef51754ef8a6240dc846ac
Ricky Nguyen <ngynrcky@gmail.com>
2025-08-06 16:17:41 -0700

    feat: adding tsconfig and fixing typescript error

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -69,6 +67,8 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+  /* Opt out of parallel tests on CI, if CI environment variable is set */
+  ...(process.env.CI ? { workers: 1 } : {}),
 
   /* Run your local dev server before starting the tests */
   // webServer: {
